328c4782d7934809631dead4d25bee15caa33718
Raphael Saldanha <rfsaldanha@gmail.com>
2023-05-02 11:46:35 +0200

    sexo argument

--- R/get_dengue.R
+++ R/get_dengue.R
@@ -3,6 +3,7 @@
 #' @param agg character. Spatial aggregation level. \code{mun_res} for municipality of residence.
 #' @param agg_time character. Time aggregation level. \code{year} for yearly data. \code{month} for monthly data. \code{week} for weekly data. Defaults to \code{year}.
 #' @param ano vector. Year of the case.
+#' @param sexo character. Sex of the case. \code{Masculino} for males, \code{Feminino} for females and \code{Ignorado} for unknown.
 #' @param psql_user character. psql username. If not provided, the function will look for it on renviron.
 #' @param psql_pwd character. psql password. If not provided, the function will look for it on renviron.
 #'
@@ -10,7 +11,7 @@
 #'
 #' @importFrom rlang .data
 #' @export
-get_dengue <- function(agg, agg_time, ano, psql_user = NULL, psql_pwd = NULL){
+get_dengue <- function(agg, agg_time, ano, sexo = NULL, psql_user = NULL, psql_pwd = NULL){
   # Function argument check
   checkmate::assert_choice(x = agg, choices = c("mun_res"))
   checkmate::assert_choice(x = agg_time, choices = c("year", "month", "week"))
@@ -63,6 +64,20 @@ get_dengue <- function(agg, agg_time, ano, psql_user = NULL, psql_pwd = NULL){
   # Filter by year
   res <- dengue_tb %>%
     dplyr::filter(.data$year %in% ano)
+
+  # Filter by sex
+  if(!is.null(sexo)){
+    if(sexo == "Masculino"){
+      res <- res %>%
+        dplyr::filter(.data$sex == "M")
+    } else if(sexo == "Feminino"){
+      res <- res %>%
+        dplyr::filter(.data$sex == "F")
+    } else if(sexo == "Ignorado"){
+      res <- res %>%
+        dplyr::filter(.data$sex == "I")
+    }
+  }
 
   # Create date variable
   if(agg_time == "year"){
