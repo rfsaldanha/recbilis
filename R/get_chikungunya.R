b113cd373e8f6674bbc1524cf30ab47995f9bfe8
Raphael Saldanha <rfsaldanha@gmail.com>
2025-03-18 13:17:47 -0300

    Update chikungunya

--- R/get_chikungunya.R
+++ R/get_chikungunya.R
@@ -11,12 +11,26 @@
 #'
 #' @importFrom rlang .data
 #' @export
-get_chikungunya <- function(agg, agg_time, ano, sexo = NULL, idade_a = NULL, idade_b = NULL){
+get_chikungunya <- function(
+  agg,
+  agg_time,
+  ano,
+  sexo = NULL,
+  idade_a = NULL,
+  idade_b = NULL
+) {
   # Function argument check
-  checkmate::assert_choice(x = agg, choices = c("uf_res", "regsaude_449_res", "mun_res"))
+  checkmate::assert_choice(
+    x = agg,
+    choices = c("uf_res", "regsaude_449_res", "mun_res")
+  )
   checkmate::assert_choice(x = agg_time, choices = c("year", "month", "week"))
   checkmate::assert_vector(x = ano)
-  checkmate::assert_choice(x = sexo, choices = c("Masculino", "Feminino", "Ignorado"), null.ok = TRUE)
+  checkmate::assert_choice(
+    x = sexo,
+    choices = c("Masculino", "Feminino", "Ignorado"),
+    null.ok = TRUE
+  )
   checkmate::assert_number(x = idade_a, lower = 0, null.ok = TRUE)
   checkmate::assert_number(x = idade_b, lower = 0, null.ok = TRUE)
 
@@ -25,7 +39,7 @@ get_chikungunya <- function(agg, agg_time, ano, sexo = NULL, idade_a = NULL, ida
   psql_schema <- get_psql_parameters()$psql_schema
 
   # Table
-  psql_table <- "chikungunyat"
+  psql_table <- "chikungunya"
 
   # Close connection
   on.exit(DBI::dbDisconnect(conn = conn))
@@ -34,14 +48,17 @@ get_chikungunya <- function(agg, agg_time, ano, sexo = NULL, idade_a = NULL, ida
   stopifnot(check_table_avail(conn = conn, table = psql_table))
 
   # Lazy table abstraction
-  dengue_tb <- dplyr::tbl(conn, DBI::Id(schema = psql_schema, table = psql_table))
+  dengue_tb <- dplyr::tbl(
+    conn,
+    DBI::Id(schema = psql_schema, table = psql_table)
+  )
 
   # Variable spatial aggregation
-  if (agg == "uf_res"){
+  if (agg == "uf_res") {
     agg <- "geocoduf"
-  } else if(agg == "regsaude_449_res"){
+  } else if (agg == "regsaude_449_res") {
     agg <- "geocodrs"
-  } else if(agg == "mun_res"){
+  } else if (agg == "mun_res") {
     agg <- "geocodmu"
   }
 
@@ -52,48 +69,48 @@ get_chikungunya <- function(agg, agg_time, ano, sexo = NULL, idade_a = NULL, ida
     dplyr::filter(.data$year %in% ano)
 
   # Filter by sex
-  if(!is.null(sexo)){
-    if(sexo == "Masculino"){
+  if (!is.null(sexo)) {
+    if (sexo == "Masculino") {
       res <- res %>%
         dplyr::filter(.data$sex == "M")
-    } else if(sexo == "Feminino"){
+    } else if (sexo == "Feminino") {
       res <- res %>%
         dplyr::filter(.data$sex == "F")
-    } else if(sexo == "Ignorado"){
+    } else if (sexo == "Ignorado") {
       res <- res %>%
         dplyr::filter(.data$sex == "I")
     }
   }
 
   # Filter by age
-  if(!is.null(idade_a) & is.null(idade_b)){
+  if (!is.null(idade_a) & is.null(idade_b)) {
     res <- res %>%
       dplyr::filter(.data$age <= idade_a)
   }
-  if(!is.null(idade_b) & is.null(idade_a)){
+  if (!is.null(idade_b) & is.null(idade_a)) {
     res <- res %>%
       dplyr::filter(.data$age <= idade_b)
   }
-  if(!is.null(idade_a) & !is.null(idade_b)){
+  if (!is.null(idade_a) & !is.null(idade_b)) {
     res <- res %>%
       dplyr::filter(.data$age >= idade_a & .data$age <= idade_b)
   }
 
   # Create date variable
-  if(agg_time == "year"){
+  if (agg_time == "year") {
     res <- res %>%
       dplyr::mutate(date = as.character(.data$year))
-  } else if(agg_time == "month"){
+  } else if (agg_time == "month") {
     res <- res %>%
       dplyr::mutate(date = paste0(.data$year, "-", .data$month))
-  } else if(agg_time == "week"){
+  } else if (agg_time == "week") {
     res <- res %>%
       dplyr::mutate(date = paste0(.data$year, "-", .data$epiweek))
   }
 
   # Group and count records by space and time aggregations
   res <- res %>%
-    dplyr::group_by(agg = rlang::sym(agg), agg_time = .data$date) %>%
+    dplyr::group_by(agg = !!rlang::sym(agg), agg_time = .data$date) %>%
     dplyr::summarise(freq = dplyr::n()) %>%
     dplyr::ungroup()
 
@@ -109,7 +126,7 @@ get_chikungunya <- function(agg, agg_time, ano, sexo = NULL, idade_a = NULL, ida
     )
 
   # Format month and week dates
-  if(agg_time %in% c("month", "week")){
+  if (agg_time %in% c("month", "week")) {
     res <- res %>%
       dplyr::mutate(
         p1 = substr(.data$agg_time, 0, 4),
@@ -117,7 +134,6 @@ get_chikungunya <- function(agg, agg_time, ano, sexo = NULL, idade_a = NULL, ida
         agg_time = paste0(.data$p1, "-", .data$p2)
       ) %>%
       dplyr::select(-"p1", -"p2")
-
   }
 
   # Return result
